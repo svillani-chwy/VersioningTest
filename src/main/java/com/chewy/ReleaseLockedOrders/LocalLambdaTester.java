bd48829c2f4858a21bf15d3aac00e730cc75fca0
gmathavadoss <gmathavadoss@chewy.com>
2019-10-01 16:53:17 -0400

    changes

--- src/main/java/com/chewy/ReleaseLockedOrders/LocalLambdaTester.java
+++ src/main/java/com/chewy/ReleaseLockedOrders/LocalLambdaTester.java
@@ -3,6 +3,5 @@ package com.chewy.ReleaseLockedOrders;
 public class LocalLambdaTester {
     public static void main(String[] args) throws Exception {
         ReleaseLockedOrdersLambda r = new ReleaseLockedOrdersLambda();
-
     }
 }
