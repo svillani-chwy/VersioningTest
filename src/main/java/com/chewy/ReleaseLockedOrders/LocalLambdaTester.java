cb224ec28ad0ebe62f45c012b9cdc89db644724b
steve <svillani@chewy.com>
2019-10-01 12:03:44 -0400

    test

--- src/main/java/com/chewy/ReleaseLockedOrders/LocalLambdaTester.java
+++ src/main/java/com/chewy/ReleaseLockedOrders/LocalLambdaTester.java
@@ -3,6 +3,5 @@ package com.chewy.ReleaseLockedOrders;
 public class LocalLambdaTester {
     public static void main(String[] args) throws Exception {
         ReleaseLockedOrdersLambda r = new ReleaseLockedOrdersLambda();
-        r.handleRequest(null, null, null);
     }
 }
